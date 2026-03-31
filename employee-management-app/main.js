2cc63321da698bfb9101b4315e1464ba940a7dc1
Butts-yn <ryanbuturu@gmail.com>
2026-03-31 13:50:29 +0300

    Update main.js

---
+++ employee-management-app/main.js
@@ -0,0 +1,10 @@
+function showEditAlert() {
+    alert("Opening editor...");
+}
+
+function confirmDelete() {
+    let confirmation = confirm("Are you sure you want to delete?");
+    if (confirmation) {
+        alert("deleted successfully.");
+    }
+}
